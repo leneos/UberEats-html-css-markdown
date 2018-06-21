28af6d0560e8c5de050b7a39c3d1b3c141124610
leneos <shakhov94@yandex.ru>
2018-06-21 13:52:18 +0300

    +some gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('css', function () {
     var postcss = require('gulp-postcss');
@@ -8,6 +11,20 @@ gulp.task('css', function () {
     return gulp.src('src/**/*.css')
         .pipe(sourcemaps.init())
         .pipe(postcss([require('precss'), require('autoprefixer')]))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('build/'));
 });
+
+gulp.task('imgmin', function () {
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'))
+});
+
+gulp.task('htmlminify', function () {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build'));
+});
+
