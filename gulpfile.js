64eb87395fe878ba4721b41894fdaf57ffaf22bd
leneos <shakhov94@yandex.ru>
2018-06-21 13:30:42 +0300

    some gulp installations

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('css', function () {
+    var postcss = require('gulp-postcss');
+    var sourcemaps = require('gulp-sourcemaps');
+
+    return gulp.src('src/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([require('precss'), require('autoprefixer')]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/'));
 });
